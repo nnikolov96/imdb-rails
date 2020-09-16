dabe598eb8a78c21875390468d3cf424577a4e0c
Nikola <nikolanenchevnikolov@gmail.com>
2020-09-16 22:58:15 +0300

    tests for reviews

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { FactoryBot.create(:review) }
+  it 'has a valid factory' do
+    expect(review).to be_valid
+  end
+  it 'is not valid with too short comment' do
+    review.comment = 'sh'
+    expect(review).to_not be_valid
+    expect(review.errors[:comment]).to include 'is too short (minimum is 4 characters)'
+  end
+  it 'is not valid when stars not in list' do
+    review.stars = 6
+    expect(review).to_not be_valid
+    expect(review.errors[:stars]).to include 'must be between 1 and 5'
+  end
 end
