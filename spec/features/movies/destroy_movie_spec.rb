356bf7bb2df7291369a1df6ebbe5a700190d366f
Nikola <nikolanenchevnikolov@gmail.com>
2020-09-16 23:24:35 +0300

    refactored movie specs

--- spec/features/movies/destroy_movie_spec.rb
+++ spec/features/movies/destroy_movie_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature 'User can destroy movies' do
-  before do
-    FactoryBot.create(:movie, title: 'Avengers')
-    visit '/'
-    click_link 'Avengers'
+  let(:user) { FactoryBot.create(:user, :admin) }
+  let!(:movie) { FactoryBot.create(:movie, title: 'Avengers') }
+
+  context 'when logged in as admin' do
+    scenario 'with success' do
+      sign_in_as(user.email, user.password)
+      visit '/'
+      click_link 'Avengers'
+      click_link 'Delete'
+      expect(page).to_not have_content 'Avengers'
+      expect(page).to have_content 'Movie was deleted'
+    end
   end
 
-  scenario 'with success' do
-    click_link 'Delete'
-    expect(page).to_not have_content 'Avengers'
-    expect(page).to have_content 'Movie was deleted'
+  context 'when logged in as normal user or not logged in' do
+    scenario 'hidden links' do
+      visit movie_path(movie)
+      expect(page).to_not have_link 'Delete'
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,17 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.include Features, type: :feature
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
